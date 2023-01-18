ac807b7abd428654038e62889fd910fcd42e39f1
mlmillanp <mlmillanp@gmail.com>
2023-01-18 16:09:13 +0100

    nuevos ejercicios de arrays

---
+++ src/main/java/com/unisys/miapp/Ejercicios_arrays.java
@@ -0,0 +1,12 @@
+package com.unisys.miapp;
+
+public class Ejercicios_arrays {
+    public static void main(String[] args) {
+        int i;
+        int[] miArray = new int[4] ;
+        for (i=0;i<miArray.length;i++)
+        {
+         System.out.println(miArray[i]);
+        }
+    }
+}
